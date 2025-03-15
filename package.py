d093e348eedab0d91710dcdaa0798fb6b8af0ff9
ffekirnew <ffekirnew0808@gmail.com>
2025-03-15 09:09:22 +0300

    Remove unneccesary message queue and persistence code

--- package.py
+++ package.py
@@ -1,8 +1,4 @@
-from threading import Thread
-
 from src.common.logging_helpers import get_logger
-from src.infrastructure.rabbitmq.producer import RabbitMQProducer
-from src.infrastructure.rabbitmq.subscriber import RabbitMQSubscriber, example_callback
 from src.webapi.api import API
 
 LOG = get_logger()
@@ -10,27 +6,15 @@ LOG = get_logger()
 
 class Package:
     def __init__(self) -> None:
-        # self._rabbitmq_producer = RabbitMQProducer("test_queue")
-        # self._rabbitmq_subscriber = RabbitMQSubscriber("test_queue", example_callback)
         self._api = API()
 
-    # def start_producer(self) -> None:
-    #     self._rabbitmq_producer.start()
-    #
-    #     self._rabbitmq_producer.publish({"title": "Hello, world!"})
-    #
-    # def start_subscriber(self) -> None:
-    #     self._rabbitmq_subscriber.start()
-
-    def start_api(self) -> None:
+    def start(self) -> None:
         self._api.start()
 
     def stop(self) -> None:
         self._api.stop()
-        # self._rabbitmq_producer.stop()
-        # self._rabbitmq_subscriber.stop()
 
 
 if __name__ == "__main__":
     main = Package()
-    main.start_api()
+    main.start()
