4905746885e9df25236bf57fc44e3c24fb7d0172
ffekirnew <ffekirnew0808@gmail.com>
2025-03-15 09:10:46 +0300

    Finalize driver feature for user management

--- src/application/features/drivers/handlers/commands/login_driver_command_handler.py
+++ src/application/features/drivers/handlers/commands/login_driver_command_handler.py
@@ -1,14 +1,11 @@
+from ed_domain_model.services.auth.dtos import UnverifiedUserDto
 from rmediator.decorators import request_handler
 from rmediator.types import RequestHandler
 
 from src.application.common.responses.base_response import BaseResponse
-from src.application.contracts.infrastructure.persistence.abc_unit_of_work import (
-    ABCUnitOfWork,
-)
-from src.application.features.drivers.dtos.driver_account_dto import DriverAccountDto
-from src.application.features.drivers.requests.commands.login_driver_command import (
-    LoginDriverCommand,
-)
+from src.application.contracts.infrastructure.api.abc_api import ABCApi
+from src.application.features.drivers.requests.commands.login_driver_command import \
+    LoginDriverCommand
 from src.common.logging_helpers import get_logger
 
 LOG = get_logger()
@@ -16,11 +13,17 @@ LOG = get_logger()
 DRIVERS_DB = {}
 
 
-@request_handler(LoginDriverCommand, BaseResponse[DriverAccountDto])
+@request_handler(LoginDriverCommand, BaseResponse[UnverifiedUserDto])
 class LoginDriverCommandHandler(RequestHandler):
-    def __init__(self, uow: ABCUnitOfWork):
-        self._uow = uow
+    def __init__(self, api: ABCApi):
+        self._api = api
 
     async def handle(
         self, request: LoginDriverCommand
-    ) -> BaseResponse[DriverAccountDto]: ...
+    ) -> BaseResponse[UnverifiedUserDto]:
+        LOG.info("Handling LoginDriverCommand")
+        unverified_user_dto = self._api.auth_api.login_get_otp({**request.dto})
+
+        return BaseResponse[UnverifiedUserDto].success(
+            "Log-in OTP sent successfully", unverified_user_dto
+        )
